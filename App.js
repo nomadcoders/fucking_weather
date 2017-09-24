302c4d8f241e20091f878c3cc508cc6cc62f30e7
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-09-24 23:45:45 +0700

    Installing Expo and creating a new Project

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello! I'm a Native Developer Now! And I LOVE REACT!!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
