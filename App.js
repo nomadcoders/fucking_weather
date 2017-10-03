4844a7a6015fd61e7c556ee854c16e1812cec2e2
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-10-03 12:17:57 +0700

    Working with Icons

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
 import Weather from "./Weather";
 
 export default class App extends Component {
@@ -10,6 +10,7 @@ export default class App extends Component {
     const { isLoaded } = this.state;
     return (
       <View style={styles.container}>
+        <StatusBar hidden={true} />
         {!isLoaded ? (
           <Weather />
         ) : (
